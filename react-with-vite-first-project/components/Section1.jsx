4e887e177c12961353c35501d8003cae03f011b8
Vandilson <vandilson25@outlook.com>
2023-10-15 12:08:27 -0300

    create Section1 component

---
+++ react-with-vite-first-project/components/Section1.jsx
@@ -0,0 +1,7 @@
+export default function Section1() {
+    return (
+        <section className="w-full h-80 bg-white flex p-5 justify-around items-center"> 
+            <p className="text-2xl text-center px-40 leading-relaxed">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas laborum quaerat cumque sint, nesciunt amet nam?</p>
+        </section>
+    );
+}
