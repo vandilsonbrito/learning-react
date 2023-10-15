076b16107cbedd7ff4f87ebba3759528a21e0742
Vandilson <vandilson25@outlook.com>
2023-10-15 12:08:56 -0300

    create footer component

---
+++ react-with-vite-first-project/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="w-full h-24 bg-slate-600 flex justify-center items-center">
+            <p className="text-4xl text-white">Footer</p>
+        </footer>
+    );
+}
