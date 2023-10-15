a893473cc2fa08577c134800d767b7203b1cfe59
Vandilson <vandilson25@outlook.com>
2023-10-15 12:08:42 -0300

    create section2 component

---
+++ react-with-vite-first-project/components/Section2.jsx
@@ -0,0 +1,8 @@
+export default function Section2() {
+    return (
+        <section className="w-full h-80 bg-white flex p-5 justify-around items-center"> 
+        <img src="../public/vite.svg" className="w-52" alt="" />
+        <img src="../src/assets/react.svg" className="w-52" alt="" />
+    </section>
+    );
+}
