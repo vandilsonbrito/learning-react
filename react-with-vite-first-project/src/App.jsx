d8ef6867746356a10d6f0d7f9b1f9b77a7f14309
Vandilson <vandilson25@outlook.com>
2023-10-15 12:09:22 -0300

    add compnents in App

--- react-with-vite-first-project/src/App.jsx
+++ react-with-vite-first-project/src/App.jsx
@@ -1,4 +1,6 @@
+import Footer from '../components/Footer'
 import Header from '../components/Header'
+import Main from '../components/Main'
 import './index.css'
 
 function App() {
@@ -6,6 +8,8 @@ function App() {
   return (
       <>
         <Header />
+        <Main/>
+        <Footer/>
       </>
   )
 }
