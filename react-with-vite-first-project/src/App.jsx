0ad23134ecb28fb801c9cec335db3a6b87a08cbe
Vandilson <vandilson25@outlook.com>
2023-10-16 15:38:04 -0300

    adding useEffect Hook

--- react-with-vite-first-project/src/App.jsx
+++ react-with-vite-first-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Main from '../components/Main'
@@ -12,14 +12,17 @@ function App() {
     setNum(num += 1);
   }
 
+  useEffect(() => {
+      console.log('Renderizou')
+  }, [num]);
 
   return (
       <>
         <Header className="w-full h-20 bg-slate-600 flex justify-between items-center px-7" />
         <Main>
             <div className="flex flex-col justify-center items-center">
-              <span>Número:{num}</span>
-              <button onClick={handleClick} className='w-20 h-10 bg-white'>Click</button>
+              <span>Número: {num}</span>
+              <button onClick={handleClick} className='w-20 h-10 bg-white translate-x-[98]'>Click</button>
             </div>
         </Main>
         <Footer className='w-full h-20 bg-slate-600 flex justify-center items-center'/>
