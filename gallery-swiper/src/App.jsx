d6c8f551c2d41dd72634d809464a1311c97088fb
vandilsonbrito <vandilson25@outlook.com>
2023-10-19 10:24:26 -0300

    Added loading icon from fontawesome

--- gallery-swiper/src/App.jsx
+++ gallery-swiper/src/App.jsx
@@ -2,6 +2,9 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
 import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
 import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+
 import 'swiper/css'; // Importe o CSS principal do Swiper
 import 'swiper/css/navigation';
 import 'swiper/swiper-bundle.css';
@@ -12,13 +15,13 @@ SwiperCore.use([Navigation, Pagination, Scrollbar]);
 
 
 export default function App() {
-  const [data, setData] = useState({ photos: [] });
+  const [data, setData] = useState({ photos: []});
   const [load, setLoad] = useState(false);
   const API_KEY = import.meta.env.VITE_API_KEY;
 
   useEffect(() => {
     getApi();
-  });
+  }, []);
 
   const getApi = async () => {
     try {
@@ -48,21 +51,24 @@ export default function App() {
           loop={true}
           spaceBetween={0}
           slidesPerView={1}
-          >
-          
-            {data.photos.map((photo, index) => (
-                <SwiperSlide className="w-full h-full overflow-hidden" key={index}>{
-                  <img 
-                  className='w-full h-full bg-center' 
-                  key={photo.id} 
-                  src={photo.src.medium} 
-                  alt={photo.photographer} 
-                  />
-                }
-                {/* <div className="swiper-button-next "></div>
-                <div className="swiper-button-prev"></div> */}
-                </SwiperSlide>
-              ))}
+          > 
+            { load && <div className='relative -left-[45%] '><FontAwesomeIcon icon={faSpinner} className='text-6xl fa-spin' /></div> }
+            {data.photos.length > 0 ? 
+              <div> {
+                  data.photos.map((photo, index) => {
+                    return (
+                    <SwiperSlide className="w-full h-full overflow-hidden" key={index}>{
+                      <img 
+                      className='w-full h-full bg-center' 
+                      key={photo.id} 
+                      src={photo.src.medium} 
+                      alt={photo.photographer} 
+                      />
+                    }
+                    </SwiperSlide> 
+                    )}
+                  )}
+              </div> : null }
 
         </Swiper>
         </div>   
